cc4f7f2dd35c0523a4f1d3c245eca4a484764be0
Andrej Jasso <andrej.jasso@goodrequest.com>
2019-06-06 17:26:31 +0200

    init

---
+++ AdoKod/ViewController.swift
@@ -0,0 +1,22 @@
+//
+//  ViewController.swift
+//  AdoKod
+//
+//  Created by Andrej Jasso on 06/06/2019.
+//  Copyright © 2019 Andrej Jasso. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        
+
+    }
+
+
+}
+
