4c86aaee9cabd5e3e77cfe1739b6c32022f97f6c
meteora8888 <33287058+meteora8888@users.noreply.github.com>
2019-06-10 14:41:31 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -3,9 +3,6 @@ import PackageDescription
 
 let package = Package(
     name: "AdoKod",
-     products: [
-        .library(name: "DangerDeps[Product name (optional)]", type: .dynamic, targets: ["AdoKod"]), // dev
-    ],
     dependencies: [
        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"), // dev
        .package(url: "https://github.com/username/DangerPlugin.git", from: "0.1.0") // dev
