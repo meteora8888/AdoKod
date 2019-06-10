6b9f1c9bdfa29988cff859f4a6a47bc6cb29d527
meteora8888 <33287058+meteora8888@users.noreply.github.com>
2019-06-10 15:41:59 +0200

    Update Dangerfile.swift

--- Dangerfile.swift
+++ Dangerfile.swift
@@ -1,4 +1,5 @@
 import Danger
+import DangerSwiftLint
 
 let danger = Danger()
 let allSourceFiles = danger.git.modifiedFiles + danger.git.createdFiles
@@ -16,3 +17,5 @@ warn("Something pretty bad, but not important enough to fail the build")
 fail("Something that must be changed")
 
 markdown("Free-form markdown that goes under the table, so you can do whatever.")
+
+SwiftLint.lint()
