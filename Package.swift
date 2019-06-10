49876bf3348bc02932dc971635c87c017d16dbfe
meteora8888 <33287058+meteora8888@users.noreply.github.com>
2019-06-10 14:40:29 +0200

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -4,13 +4,13 @@ import PackageDescription
 let package = Package(
     name: "AdoKod",
      products: [
-        .library(name: "DangerDeps[Product name (optional)]", type: .dynamic, targets: ["DangerDependencies"]), // dev
+        .library(name: "DangerDeps[Product name (optional)]", type: .dynamic, targets: ["AdoKod"]), // dev
     ],
     dependencies: [
        .package(url: "https://github.com/danger/swift.git", from: "1.0.0"), // dev
        .package(url: "https://github.com/username/DangerPlugin.git", from: "0.1.0") // dev
     ],
     targets: [
-        .target(name: "AdoKod", dependencies: ["Danger"], path: "Ado", sources: ["Stringify.swift"]),
+        .target(name: "AdoKod", dependencies: ["Danger","DangerPlugin"], path: "Ado", sources: ["Stringify.swift"]),
     ]
 )
